d8b70a7aa8c71328f8b54db92f5a561b0e9b61c9
Erin Boehlert <eringraceboehlert@gmail.com>
2019-02-10 18:58:58 -0500

    add prop types

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import Book from './Book';
+import PropTypes from 'prop-types';
 
 export default class BookShelf extends Component {
+    static propTypes = {
+        books: PropTypes.array.isRequired,
+        refreshBooks: PropTypes.func.isRequired
+    }
+
     render() {
         const { books, refreshBooks } = this.props;
         return (
             <div>
-                <div className="bookshelf">
+                <div className='bookshelf-container'>
                     {books.map(book => (
                         <Book key={book.id} book={book} onShelfChange={refreshBooks}/>
                     ))}
